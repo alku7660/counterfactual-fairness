decd1e55908198767db47baec7a97829b51eff17
alku7660 <alejandro.kuratomi@dsv.su.se>
2022-04-26 14:26:21 +0200

    Testing metrics differences plot continuing

--- fairness_evaluator.py
+++ fairness_evaluator.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.filterwarnings("ignore")
 import numpy as np
 import pandas as pd
 import pickle
@@ -12,25 +14,30 @@ def load_obj(file_name):
     """
     Method to read an Evaluator object containing the evaluation results for all the instances of a given dataset
     """
-    with open(results_cf_obj_dir+file_name, 'rb') as input:
+    with open(results_cf_obj_dir+file_name+'_eval.pkl', 'rb') as input:
         evaluator_obj = pickle.load(input)
     return evaluator_obj
 
-def extract_x_cd_df(eval_cf_df,eval_x_df):
+def extract_x_cd_df(eval_cf_df,eval_x_df, cf_metrics):
     """
     Method that joins accuracy, proximity and sparsity from the eval object and outputs the join as a single dataframe
     Input eval_cf_df: DataFrame with all CF
     Input eval_x_df: DataFrame containing all the instances of interest information
     Output full_df: DataFrame containing all the instances of interest and corresponding CFs information
     """
-    full_df = pd.concat((eval_x_df, eval_cf_df),axis=1)
-    full_df_cf_list = full_df['cf'].tolist()
-    full_df_x_list = full_df['x'].tolist()
-    full_df_method = full_df['cf_method']
-    full_df_metrics = full_df[['proximity','sparsity','accuracy']]
+    eval_cf_df.dropna(inplace=True)
+    full_df_cf_list = eval_cf_df['cf'].tolist()
+    full_df_x_list = eval_x_df['x'].tolist()
     cf_df = pd.concat(full_df_cf_list)
     x_df = pd.concat(full_df_x_list)
-    cf_df = pd.concat((full_df_method,cf_df,full_df_metrics),axis=1)
+    cf_df = cf_df.reset_index(drop=True)
+    full_df_metrics = eval_cf_df[['cf_method']+cf_metrics]
+    full_df_metrics = full_df_metrics.reset_index(drop=True)
+    x_df = x_df.reset_index(drop=True)
+    full_x_metrics = eval_x_df['accuracy']
+    full_x_metrics = full_x_metrics.reset_index(drop=True)
+    cf_df = pd.concat((cf_df,full_df_metrics),axis=1)
+    x_df = pd.concat((x_df,full_x_metrics),axis=1)
     return x_df, cf_df
 
 def extract_values_labels(cf_df, method, feat, feat_val, metric, protected_feat):
@@ -41,7 +48,7 @@ def extract_values_labels(cf_df, method, feat, feat_val, metric, protected_feat)
     feat_metric_method_values = cf_df[(cf_df['cf_method'] == method) & (cf_df[feat] == feat_val)][metric]
     return feat_metric_method_values, method+':'+feat_val_name
 
-def metric_differences_plot(datasets, methods_to_run, metrics):
+def metric_differences_plot(datasets, methods_to_run, cf_metrics):
     """
     Method that plots the metric differences among features, datasets and methods
     """
@@ -51,10 +58,10 @@ def metric_differences_plot(datasets, methods_to_run, metrics):
         eval_cf_df = eval_obj.all_cf_data
         protected_feat = eval_obj.feat_protected
         protected_feat_keys = list(protected_feat.keys())
-        x_df, cf_df = extract_x_cd_df(eval_cf_df, eval_x_df)
+        x_df, cf_df = extract_x_cd_df(eval_cf_df, eval_x_df, cf_metrics)
         for feat in x_df.columns:
             feat_unique_val = x_df[feat].unique()
-            for metric in metrics:
+            for metric in cf_metrics:
                 metric_feat_list = []
                 metric_feat_labels = []
                 prot_feat_found = False
@@ -71,10 +78,10 @@ def metric_differences_plot(datasets, methods_to_run, metrics):
                         metric_feat_labels.append(box_label)
                 if prot_feat_found:
                     plt.boxplot(metric_feat_list, notch=True, vert=False, labels = metric_feat_labels)
-                    plt.savefig(results_cf_plots_dir+f'{data_str}_{feat}_{metric}_fairness_.png',dpi=400)
+                    plt.savefig(results_cf_plots_dir+f'{data_str}_{feat}_{metric}_fairness.png',dpi=400)
 
-datasets = ['compass','credit','adult','german','heart']  # Name of the dataset to be analyzed ['synthetic_severe_disease','synthetic_athlete','ionosphere','compass','credit','adult','german','heart']
+datasets = ['compass','german']  # Name of the dataset to be analyzed ['compass','credit','adult','german','heart']
 methods_to_run = ['nn','mo','ft','rt','gs','face','dice','mace','cchvae','juice'] #['nn','mo','ft','rt','gs','face','dice','mace','cchvae','juice']
-metrics = ['proximity','sparsity','accuracy']
+cf_metrics = ['proximity','sparsity']
 
-metric_differences_plot(datasets, methods_to_run, metrics)
+metric_differences_plot(datasets, methods_to_run, cf_metrics)
