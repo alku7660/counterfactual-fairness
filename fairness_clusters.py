fda334f7f751fc2bb81ba2b84ff5e8db479170d0
alku7660 <alejandro.kuratomi@dsv.su.se>
2024-01-16 16:37:50 +0100

    Dist running now only

--- fairness_clusters.py
+++ fairness_clusters.py
@@ -13,8 +13,8 @@ import numpy as np
 from sklearn.metrics import f1_score
 from support import save_obj
 
-datasets = ['synthetic_athlete','compass','german','student'] # 'oulad','bank','law','credit','adult','kdd_census','diabetes','synthetic_disease' Student runs ARES at 0.1, FACTS 0.4 'german','dutch','compass','synthetic_athlete','heart','student','oulad','bank','law','credit','adult','kdd_census','diabetes','synthetic_disease'
-methods_to_run = ['FOCE_dev_like','FOCE_dev_eff'] # ['FOCE_dist','FOCE_l','FOCE_e','ARES','FACTS']
+datasets = ['compass','german','student'] # 'oulad','bank','law','credit','adult','kdd_census','diabetes','synthetic_disease' Student runs ARES at 0.1, FACTS 0.4 'german','dutch','compass','synthetic_athlete','heart','student','oulad','bank','law','credit','adult','kdd_census','diabetes','synthetic_disease'
+methods_to_run = ['FOCE_dev_dist'] # ['FOCE_dist','FOCE_l','FOCE_e','ARES','FACTS']
 step = 0.01                # Step size to change continuous features
 train_fraction = 0.7       # Percentage of examples to use for training
 n_feat = 50                # Number of examples to generate synthetically per feature
