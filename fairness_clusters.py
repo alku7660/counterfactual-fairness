ee78c2b8022d4362d5a887f15bbb272988360728
alku7660 <alejandro.kuratomi@dsv.su.se>
2024-02-08 10:44:22 +0100

    Update fairness_clusters.py

--- fairness_clusters.py
+++ fairness_clusters.py
@@ -12,7 +12,7 @@ import numpy as np
 from sklearn.metrics import f1_score
 from support import save_obj
 
-datasets = ['oulad','bank','student','diabetes','law','credit','dutch','adult','kdd_census'] # 'oulad','bank','law','credit','adult','kdd_census','diabetes','synthetic_disease' Student runs ARES at 0.1, FACTS 0.4 'german','dutch','compass','synthetic_athlete','heart','student','oulad','bank','law','credit','adult','kdd_census','diabetes','synthetic_disease'
+datasets = ['oulad','bank','student','diabetes','law','credit','dutch','adult','kdd_census'] # 'compass','synthetic_athlete','german','oulad','bank','student','diabetes','law','credit','dutch','adult','kdd_census'
 methods_to_run = ['BIGRACE_dist'] # ['BIGRACE_dist','BIGRACE_l','BIGRACE_e','BIGRACE_dev_dist','BIGRACE_dev_like','BIGRACE_dev_eff','ARES','FACTS']
 step = 0.01                # Step size to change continuous features
 train_fraction = 0.7       # Percentage of examples to use for training
